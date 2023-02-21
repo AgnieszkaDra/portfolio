0e15094a331058b37806dad9e020e7ad96b035a1
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-02-21 11:10:15 +0100

    change Coderoad Course to Akademia Samouka

--- app.js
+++ app.js
@@ -104,6 +104,24 @@ const portfolioItemsNumber = portfolioItems.length
 projectsAmount.innerHTML = portfolioItemsNumber + '+'
 
 
+let imgElement = document.getElementById('imageSrc');
+let inputElement = document.getElementById('fileInput');
+inputElement.addEventListener('change', (e) => {
+  imgElement.src = URL.createObjectURL(e.target.files[0]);
+}, false);
+imgElement.onload = function() {
+  let mat = cv.imread(imgElement);
+  cv.imshow('canvasOutput', mat);
+  mat.delete();
+};
+var Module = {
+  // https://emscripten.org/docs/api_reference/module.html#Module.onRuntimeInitialized
+  onRuntimeInitialized() {
+    document.getElementById('status').innerHTML = 'OpenCV.js is ready.';
+  }
+};
+
+
 
 
 
