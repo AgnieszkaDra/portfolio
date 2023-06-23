da3fa3ae0e01fdf035a781a47cb625e9fa4de651
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-06-24 00:32:33 +0200

    some changes in forwardPage function

--- app.js
+++ app.js
@@ -66,15 +66,23 @@ pageTransitions(contactBtns, secContact)
 
 
 function forwardPage(e) {
+
+   
     const id = e.target;
+    const header = id.closest('.header')
+ 
     const page = document.querySelector('#portfolios')
-    const parentPage = page.parentElement
-    const controlPage = document.querySelector('[data-id="portfolio"]')
-    const controlId = document.querySelector('[data-id="home"]')
+    const parentPage = page.parentElement.parentElement
+   
+    const controlPage = parentPage.querySelector('[data-id="portfolio"]')
+  
+    const controlId = parentPage.querySelector('[data-id="home"]')
+   
 
     if (id) {
         controlId.classList.remove('active-btn')
         controlPage.classList.add('active-btn')
+        header.classList.remove('active')
         parentPage.classList.add('active')
     }
 }
