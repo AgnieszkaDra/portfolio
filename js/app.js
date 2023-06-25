adfe47949a81c4f4187325decf8671d0c64d0fd1
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-06-25 09:49:54 +0200

    delete some variables

--- js/app.js
+++ js/app.js
@@ -5,16 +5,16 @@ const sections = document.querySelectorAll('.section')
 const sectBtns = document.querySelectorAll('.controlls')
 const sectBtn = document.querySelectorAll('.control')
 const secHeader = document.querySelector('.section.header')
-const homeControlls = document.querySelector('.controlls.home')
+
 const homeBtns = homeControlls.querySelectorAll('.control')
 const secAbout = document.querySelector('.section.about')
-const aboutControlls = document.querySelector('.controlls.about')
-const aboutBtns = aboutControlls.querySelectorAll('.control')
+
+
 const secPortfolio = document.querySelector('.section.portfolio')
 const portfolioControlls = document.querySelector('.controlls.portfolio')
-const portfolioBtns = portfolioControlls.querySelectorAll('.control')
+
 const secContact = document.querySelector('.section.contact')
-const contactControlls = document.querySelector('.controlls.contact')
+
 const contactBtns = contactControlls.querySelectorAll('.control')
 const allSections = document.querySelector('.main-content')
 const toggleIcon = document.getElementById('toggle-icon')
@@ -37,29 +37,7 @@ console.log(sections)
 
 
 
-// function forwardPage(e) {
 
-   
-//     const id = e.target;
-//     const header = id.closest('.header')
- 
-//     const page = document.querySelector('#portfolios')
-//     const parentPage = page.parentElement.parentElement
-   
-//     const controlPage = parentPage.querySelector('[data-id="portfolio"]')
-  
-//     const controlId = parentPage.querySelector('[data-id="home"]')
-   
-
-//     if (id) {
-//         controlId.classList.remove('active-btn')
-//         controlPage.classList.add('active-btn')
-//         header.classList.remove('active')
-//         parentPage.classList.add('active')
-//     }
-// }
-
-// portfolioLink.addEventListener('click', forwardPage)
 
 const toogleSwitch = document.querySelector('input[type="checkbox"]')
 
